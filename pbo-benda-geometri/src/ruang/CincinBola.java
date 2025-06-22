a49d612a039823c10b333b1bcd99be201ef3ebce
goodman97 <151006064+goodman97@users.noreply.github.com>
2025-06-23 04:33:10 +0700

    Update CincinBola.java

--- pbo-benda-geometri/src/ruang/CincinBola.java
+++ pbo-benda-geometri/src/ruang/CincinBola.java
@@ -3,12 +3,10 @@ package ruang;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class CincinBola extends Bola implements Runnable {
+public class CincinBola extends Bola {
     private double jariJariDalam;
-    private double volume;
-    private double luasPermukaan;
-    private Thread thread;
-    private String namaProses;
+    private double volumeCincin;
+    private double luasPermukaanCincin;
 
     public CincinBola(double jariJariLuar, double jariJariDalam) {
         super(jariJariLuar);
@@ -16,7 +14,8 @@ public class CincinBola extends Bola implements Runnable {
             throw new IllegalArgumentException("Jari-jari dalam harus lebih dari 0 dan lebih kecil dari jari-jari luar.");
         }
         this.jariJariDalam = jariJariDalam;
-        this.namaProses = "Perhitungan Cincin Bola";
+        this.volumeCincin = hitungVolume();
+        this.luasPermukaanCincin = hitungLuasPermukaan();
     }
 
     @Override
@@ -26,91 +25,67 @@ public class CincinBola extends Bola implements Runnable {
 
     @Override
     public double hitungVolume() {
-        volume = (4.0 / 3.0) * Math.PI * 
-                (Math.pow(jariJari, 3) - Math.pow(jariJariDalam, 3));
-        return volume;
+        volumeCincin = (4.0 / 3.0) * Math.PI * (Math.pow(jariJari, 3) - Math.pow(jariJariDalam, 3));
+        return volumeCincin;
     }
 
-    // Overload
-    public double hitungVolume(double newJariJariLuar, double newJariJariDalam) {
-        volume = (4.0 / 3.0) * Math.PI * 
-                (Math.pow(newJariJariLuar, 3) - Math.pow(newJariJariDalam, 3));
-        return volume;
+    public double hitungVolume(double jariLuar, double jariDalam) {
+        return (4.0 / 3.0) * Math.PI * (Math.pow(jariLuar, 3) - Math.pow(jariDalam, 3));
     }
 
     @Override
     public double hitungLuasPermukaan() {
-        luasPermukaan = 4 * Math.PI * 
-                (Math.pow(jariJari, 2) - Math.pow(jariJariDalam, 2));
-        return luasPermukaan;
+        luasPermukaanCincin = 4 * Math.PI * (Math.pow(jariJari, 2) - Math.pow(jariJariDalam, 2));
+        return luasPermukaanCincin;
     }
 
-    // Overload
-    public double hitungLuasPermukaan(double newJariJariLuar, double newJariJariDalam) {
-        luasPermukaan = 4 * Math.PI * 
-                (Math.pow(newJariJariLuar, 2) - Math.pow(newJariJariDalam, 2));
-        return luasPermukaan;
+    public double hitungLuasPermukaan(double jariLuar, double jariDalam) {
+        return 4 * Math.PI * (Math.pow(jariLuar, 2) - Math.pow(jariDalam, 2));
     }
 
     public void prosesInputDanValidasi() {
-        Scanner inp = new Scanner(System.in);
-        while (true) {
-            try {
-                System.out.print("Masukkan jari-jari luar bola: ");
-                double newJariJariLuar = inp.nextDouble();
-                System.out.print("Masukkan jari-jari dalam bola: ");
-                double newJariJariDalam = inp.nextDouble();
-                inp.nextLine();
+        try (Scanner inp = new Scanner(System.in)) {
+            while (true) {
+                try {
+                    System.out.print("Masukkan jari-jari luar bola: ");
+                    double jariLuar = inp.nextDouble();
+                    System.out.print("Masukkan jari-jari dalam bola: ");
+                    double jariDalam = inp.nextDouble();
+                    inp.nextLine();
 
-                if (newJariJariLuar <= 0 || newJariJariDalam <= 0) {
-                    System.out.println("Nilai jari-jari harus lebih dari 0.\n");
-                    continue;
+                    if (jariLuar <= 0 || jariDalam <= 0) {
+                        System.out.println("Nilai jari-jari harus lebih dari 0.\n");
+                        continue;
+                    }
+                    if (jariDalam >= jariLuar) {
+                        System.out.println("Jari-jari dalam harus lebih kecil dari jari-jari luar.\n");
+                        continue;
+                    }
+
+                    this.jariJari = jariLuar;
+                    this.jariJariDalam = jariDalam;
+                    this.volumeCincin = hitungVolume(jariLuar, jariDalam);
+                    this.luasPermukaanCincin = hitungLuasPermukaan(jariLuar, jariDalam);
+
+                    System.out.printf("\nVolume Cincin Bola: %.2f\n", volumeCincin);
+                    System.out.printf("Luas Permukaan Cincin Bola: %.2f\n", luasPermukaanCincin);
+                    break;
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Input tidak valid. Silakan masukkan angka.\n");
+                    inp.nextLine(); // clear buffer
                 }
-                if (newJariJariDalam >= newJariJariLuar) {
-                    System.out.println("Jari-jari dalam harus lebih kecil dari jari-jari luar.\n");
-                    continue;
-                }
-
-                super.jariJari = newJariJariLuar;
-                this.jariJariDalam = newJariJariDalam;
-                this.volume = hitungVolume(newJariJariLuar, newJariJariDalam);
-                this.luasPermukaan = hitungLuasPermukaan(newJariJariLuar, newJariJariDalam);
-                break;
-
-            } catch (InputMismatchException e) {
-                System.out.println("Input harus berupa angka.\n");
-                inp.nextLine();
             }
         }
     }
 
-    public double getVolume() {
-        return volume;
+    // Getter
+    public double getVolumeCincin() {
+        return volumeCincin;
     }
 
-    public double getLuasPermukaan() {
-        return luasPermukaan;
-    }
-
-    public void startCalculationThread() {
-        if (thread == null) {
-            thread = new Thread(this, namaProses);
-            thread.start();
-        }
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Thread " + namaProses + " mulai...");
-        System.out.println("Hitung: " + getNama());
-        System.out.printf("Volume Cincin Bola: %.2f\n", hitungVolume());
-        System.out.printf("Luas Permukaan Cincin Bola: %.2f\n", hitungLuasPermukaan());
-        System.out.println("Thread " + namaProses + " selesai.\n");
-        thread = null;
-    }
-
-    public Thread getThread() {
-        return thread;
+    public double getLuasPermukaanCincin() {
+        return luasPermukaanCincin;
     }
 
     public double getJariJariDalam() {
